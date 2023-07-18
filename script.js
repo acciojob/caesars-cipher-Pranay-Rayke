96b200a74761c27b56cfbf51b53e063cdcfff429
Pranay Rayke <120271377+Pranay-Rayke@users.noreply.github.com>
2023-07-18 10:00:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -32,10 +32,11 @@ const lookup = {
 };
 
 function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
+  //let decodedArr = []; // Your Result goes here
   // Only change code below this line
-
-  return; //return decodedArr
+   let firstCapitalLetterAscii = "A".charAtCode(0);
+	console.log(firstCapitalLetterAscii);
+  //return; //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
